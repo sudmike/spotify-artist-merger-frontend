c4547b2d1d59a0c63c73a78de9c986385d818015
sudmike <mike.sudhaus@gmail.com>
2020-10-28 12:19:36 +0100

    set up frontend structure

---
+++ src/app/create-playlist/artist-output/artist-output.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-output',
+  templateUrl: './artist-output.component.html',
+})
+export class ArtistOutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
